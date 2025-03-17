1468d32684d315c5c12ca2671cd63fcc133d13b4
Marc-Andre Bilodeau <marcandrebilodeau2002@hotmail.com>
2025-03-17 14:49:48 -0400

    Initial Website template created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './components/pages/home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Yo</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact Component={Home} />
+          <Route path='/services' Component={Services}/>
+          <Route path='/products' Component={Products}/>
+          <Route path='/sign-up' Component={SignUp}/>
+        </Routes>
+      </Router>
+    </>
   )
 }
 

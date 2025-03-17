1370f2e2467fdf495583d5571ae8b141dccae966
Marc-Andre Bilodeau <marcandrebilodeau2002@hotmail.com>
2025-03-17 15:16:37 -0400

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  baseUrl: '/portfolio_mab/',
   plugins: [react()],
-})
+  base: '/portfolio_mab/',
+});

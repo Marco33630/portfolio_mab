ea8b03047db9af8da2b40bada8c3940d77f4a2e7
Marc-Andre Bilodeau <marcandrebilodeau2002@hotmail.com>
2025-03-17 15:12:06 -0400

    Deploy test

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Home from './components/pages/home';
+import Home from './components/pages/Home';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
